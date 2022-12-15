fb27cbe452ad24116b615e53319d974d6b5b6c0d
NataliyaShapovalova <120041047+NataliyaShapovalova@users.noreply.github.com>
2022-12-15 13:59:26 +0000

    random

--- .vscode/Example003_sum/Program.cs
+++ .vscode/Example003_sum/Program.cs
@@ -1,6 +1,6 @@
 // See https://aka.ms/new-console-template for more information
-int numberA=31;
-int numberB=5;
+int numberA=new Random().Next(1,10);//1 2 3 4 5...9
+int numberB=new Random().Next (1.10);
 int result=numberA+numberB;
 Console.WriteLine(result);
 
